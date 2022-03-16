dc6df02fe463b76c2260fbf5a670b9f206b6c529
Frédéric Gardes <frederic.gardes@orange.com>
2022-03-16 14:33:09 +0100

    python: log on output by default
    Format the software description.
    Fix the licence file name.

--- python/its_client/logger/logger.py
+++ python/its_client/logger/logger.py
@@ -4,29 +4,30 @@
 #
 # This software is distributed under the MIT license, see LICENSE.txt file for more details.
 #
-# Author: Frédéric GARDES <frederic.gardes@orange.com> et al. Software description: This Intelligent Transportation
-# Systems (ITS) [MQTT](https://mqtt.org/) client based on the [JSon](https://www.json.org) [ETSI](
-# https://www.etsi.org/committee/its) specification transcription provides a ready to connect project for the
-# mobility (connected and autonomous vehicles, road side units, vulnerable road users,...).
+# Author: Frédéric GARDES <frederic.gardes@orange.com> et al.
+# Software description: This Intelligent Transportation Systems (ITS)
+# [MQTT](https://mqtt.org/) client based on the [JSon](https://www.json.org)
+# [ETSI](https://www.etsi.org/committee/its) specification transcription provides a ready to connect project
+# for the mobility (connected and autonomous vehicles, road side units, vulnerable road users,...).
 import logging
 import logging.handlers
-import time
+import sys
 from pathlib import Path
 
 
-def filter_monitoring_message(record):
+def filter_monitoring(record):
     return record.funcName == "monitore_cam" or record.funcName == "monitore_denm"
 
 
-def filter_its_message(record):
+def filter_reception(record):
     return record.funcName == "record"
 
 
-def filter_creation_message(record):
+def filter_sending(record):
     return record.funcName == "create_cam" or record.funcName == "create_denm"
 
 
-def filter_default_message(record):
+def filter_default(record):
     return (
         not record.funcName.startswith("create_")
         and not record.funcName == "record"
@@ -34,51 +35,48 @@ def filter_default_message(record):
     )
 
 
-def log_setup(log_level=logging.WARNING):
-    Path("/var/log/its_client").mkdir(parents=True, exist_ok=True)
+def log_setup(directory: str = "/data", log_level=logging.WARNING):
+    path = Path(directory + "/its_client")
+    path.mkdir(parents=True, exist_ok=True)
     # monitoring
-    monitoring_logger = logging.getLogger("obu.monitoring")
+    monitoring_logger = logging.getLogger("monitoring")
     monitoring_handler = logging.handlers.RotatingFileHandler(
-        filename="/var/log/its_client/monitoring-obu.csv",
+        filename=path / "monitoring.csv",
         maxBytes=2000000,
         backupCount=10,
     )
     monitoring_logger.addHandler(monitoring_handler)
-    monitoring_logger.addFilter(filter_monitoring_message)
+    monitoring_logger.addFilter(filter_monitoring)
     monitoring_logger.setLevel(log_level)
 
-    # its
-    its_logger = logging.getLogger("obu.its")
-    its_handler = logging.handlers.RotatingFileHandler(
-        filename="/var/log/its_client/its-obu.log", maxBytes=200000000, backupCount=10
+    # reception
+    reception_logger = logging.getLogger("reception")
+    reception_handler = logging.handlers.RotatingFileHandler(
+        filename=path / "reception.txt", maxBytes=200000000, backupCount=10
     )
-    its_logger.addHandler(its_handler)
-    its_handler.addFilter(filter_its_message)
-    its_handler.setLevel(log_level)
+    reception_logger.addHandler(reception_handler)
+    reception_handler.addFilter(filter_reception)
+    reception_handler.setLevel(log_level)
 
-    # creation
-    creation_logger = logging.getLogger("obu.creation")
-    creation_handler = logging.handlers.RotatingFileHandler(
-        filename="/var/log/its_client/creation-obu.log",
+    # sending
+    sending_logger = logging.getLogger("sending")
+    sending_handler = logging.handlers.RotatingFileHandler(
+        filename=path / "sending.txt",
         maxBytes=2000000,
         backupCount=10,
     )
-    creation_logger.addHandler(creation_handler)
-    creation_handler.addFilter(filter_creation_message)
-    creation_handler.setLevel(log_level)
+    sending_logger.addHandler(sending_handler)
+    sending_handler.addFilter(filter_sending)
+    sending_handler.setLevel(log_level)
 
     # default log
     logger = logging.getLogger()
-    # log_handler = logging.StreamHandler()
-    log_handler = logging.handlers.RotatingFileHandler(
-        filename="/var/log/its_client/service.log", maxBytes=200000000, backupCount=10
+    logger_handler = logging.StreamHandler(stream=sys.stdout)
+    # this is just to make the output look nice
+    logger_formatter = logging.Formatter(
+        fmt="%(asctime)s %(name)s.%(levelname)s: %(message)s"
     )
-    log_formatter = logging.Formatter(
-        "%(asctime)s.%(msecs)03d  obu [ %(thread)d %(threadName)s]: %(message)s",
-        "%b %d %H:%M:%S",
-    )
-    log_formatter.converter = time.gmtime  # UTC time
-    log_handler.setFormatter(log_formatter)
-    logger.addHandler(log_handler)
-    log_handler.addFilter(filter_default_message)
+    logger_handler.setFormatter(logger_formatter)
+    logger.addHandler(logger_handler)
+    logger_handler.addFilter(filter_default)
     logger.setLevel(log_level)
