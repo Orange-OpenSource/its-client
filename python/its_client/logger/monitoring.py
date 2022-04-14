05a5e01d2c65c38f4887c8d7d8ecebbd0ba66b7b
Nicolas Buffon <nicolas.buffon@orange.com>
2022-04-14 10:51:26 +0200

    python: add CPM monitoring function

--- python/its_client/logger/monitoring.py
+++ python/its_client/logger/monitoring.py
@@ -48,3 +48,20 @@ def monitore_denm(
         f" {station_id}/{originating_station_id}/{sequence_number}/{reference_time}/{detection_time}"
         f"/lat:{latitude}/lng:{longitude} at {timestamp}"
     )
+
+
+def monitore_cpm(
+    vehicle_id: str,
+    direction: str,
+    station_id: int,
+    generation_delta_time: int,
+    latitude: float,
+    longitude: float,
+    timestamp: int,
+    partner: str,
+    root_queue: str,
+):
+    logging.getLogger("monitoring").info(
+        f"{vehicle_id} cpm {direction} {partner}/{root_queue}"
+        f" {station_id}/{generation_delta_time}/lat:{latitude}/lng:{longitude} at {timestamp} "
+    )
