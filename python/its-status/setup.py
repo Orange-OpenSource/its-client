da48878a23861a37c2a543af1ae4a2b01ebbafe5
Yann E. MORIN <yann.morin@orange.com>
2023-01-26 16:45:20 +0100

    python/its-status: add missing dependency
    Commit 8fa85d54db84 (python/its-status: preliminary, basic client to
    send status messages) introduced a dependency on the external python
    module psutil, but forgot to add it as a dependency in the setup.py.
    
    Fix that now.
    
    Reported-by: Frederic GARDES <frederic.gardes@orange.com>
    Signed-off-by: Yann E. MORIN <yann.morin@orange.com>

--- python/its-status/setup.py
+++ python/its-status/setup.py
@@ -32,6 +32,7 @@ setup(
     platforms="LINUX",
     install_requires=[
         "paho-mqtt==1.6.1",
+        "psutil==5.8.0",
     ],
     entry_points={"console_scripts": ["its-status = its_status.main:main"]},
 )
