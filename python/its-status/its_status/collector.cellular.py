Orange-OpenSource/its-client

commit 295544cefde1841b7947e7f8aa53cdcb6847df5f

python/its-status: plugins only accept config as keyword parameter
Also prefix any unused parameter with the traditional underscore.

Signed-off-by: Yann E. MORIN <yann.morin@orange.com>

python/its-status/its_status/collector.cellular.py
@@ -8,7 +8,7 @@ from its_status import helpers
 
 
 class Status:
-    def __init__(self, cfg):
+    def __init__(self, *, _cfg):
         self.data = list()
 
     def capture(self):

python/its-status/its_status/collector.system.py
@@ -9,7 +9,7 @@ from its_status import helpers
 
 
 class Status:
-    def __init__(self, cfg):
+    def __init__(self, *, _cfg):
         self.data = None
         hw = None
 

python/its-status/its_status/collector.gnss.py
@@ -10,7 +10,7 @@ import time
 
 
 class Status:
-    def __init__(self, cfg):
+    def __init__(self, *, cfg):
         self.cfg = cfg["gnss"]
         self.data = None
         self.version = None
@@ -189,7 +189,7 @@ if __name__ == "__main__":
 
     try:
         gnss = Status(
-            {
+            cfg={
                 "gnss": {
                     "host": sys.argv[1],
                     "port": sys.argv[2],

python/its-status/its_status/collector.static.py
@@ -5,7 +5,7 @@
 
 
 class Status:
-    def __init__(self, cfg):
+    def __init__(self, *, cfg):
         self.data = {
             "version": "1.1.0",
             "type": "status",

python/its-status/its_status/collector.time_sources.py
@@ -18,7 +18,7 @@ SRC_STATE = {
 
 
 class Status:
-    def __init__(self, cfg):
+    def __init__(self, *, cfg):
         self.data = list()
         self.validity = cfg.getfloat("timesources", "validity", fallback=10.0)
         self.last_valid = 0.0

python/its-status/its_status/emitter.mqtt.py
@@ -8,7 +8,7 @@ import paho.mqtt.client
 
 
 class Status:
-    def __init__(self, cfg):
+    def __init__(self, *, cfg):
         self.enabled = cfg.get("mqtt", "enabled", fallback=True)
         if self.enabled:
             self.host = cfg.get("mqtt", "host", fallback="127.0.0.1")

python/its-status/its_status/emitter.stdout.py
@@ -7,7 +7,7 @@ import json
 
 
 class Status:
-    def __init__(self, cfg):
+    def __init__(self, *, cfg):
         self.enabled = cfg.getboolean("stdout", "enabled", fallback=False)
 
     def emit(self, data):

commit e7588e3d681300504d99fe120c10b14e7e31555b

python/its-status: fix API of {cellular,system} collectors
As the configuration is unused by those two collectors, the 'cfg'
parameter was prefixed with an underscore.

However, this is incorrect: it is a keyword-only parameter, so it can't
be underscore-prefixed, or caller would get a TypeError when they try to
instanciate those collectors:

    [...]
      File "/usr/lib/python3.10/site-packages/its_status/__init__.py", line 56, in init
        "obj": mod.Status(*args, **kwargs),
    TypeError: Status.__init__() got an unexpected keyword argument 'cfg'

Fix that by renaming the keyword-only parameter to the non-prefix 'cfg'.

Fixes: 295544c

Note: only positional parameters can be underscore-prefixed to denote
that they are unused.

Signed-off-by: Yann E. MORIN <yann.morin@orange.com>

python/its-status/its_status/collector.cellular.py
@@ -8,7 +8,7 @@ from its_status import helpers
 
 
 class Status:
-    def __init__(self, *, _cfg):
+    def __init__(self, *, cfg):
         pass
 
     def capture(self):

python/its-status/its_status/collector.system.py
@@ -9,7 +9,7 @@ from its_status import helpers
 
 
 class Status:
-    def __init__(self, *, _cfg):
+    def __init__(self, *, cfg):
         hw = None
 
         # This is the ugly-dirty code style unamically dictated by black