1c8f893fe42a4c5be8a8ef9592c7df3f43e3c81f
Yann E. MORIN <yann.morin@orange.com>
2023-03-07 15:49:59 +0100

    python/its-quadkeys: new package to abstract use of quadkeys
    Signed-off-by: Yann E. MORIN <yann.morin@orange.com>

---
+++ python/its-quadkeys/its_quadkeys/__init__.py
@@ -0,0 +1,6 @@
+# Software Name: its-quadkeys
+# SPDX-FileCopyrightText: Copyright (c) 2023 Orange
+# SPDX-License-Identifier: MIT
+# Author: Yann E. MORIN <yann.morin@orange.com>
+
+from .quadkeys import QuadKey, QuadZone
---
+++ python/its-quadkeys/setup.py
@@ -0,0 +1,33 @@
+# Software Name: its-quadkeys
+# SPDX-FileCopyrightText: Copyright (c) 2023 Orange
+# SPDX-License-Identifier: MIT
+# Author: Yann E. MORIN <yann.morin@orange.com>
+
+from setuptools import setup, find_packages
+
+setup(
+    name="its-quadkeys",
+    version="0.0.0",
+    author="Yann E. MORIN",
+    author_email="yann(dot)morin(at)orange(dot)com",
+    maintainer="Yann E. MORIN",
+    maintainer_email="yann(dot)morin(at)orange(dot)com",
+    keywords="network, its, vehicle, mqtt, etsi",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Telecommunications Industry",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Communications",
+        "Topic :: Software Development :: Embedded Systems",
+    ],
+    url="https://github.com/Orange-OpenSource/its-client",
+    packages=find_packages(exclude=["*.tests.*"]),
+    include_package_data=True,
+    description="The Intelligent Transportation Systems (ITS) [MQTT](https://mqtt.org/) Python packages based on the "
+    "[JSon](https://www.json.org) [ETSI](https://www.etsi.org/committee/its) specification transcription.",
+    long_description="The Intelligent Transportation Systems (ITS) [MQTT](https://mqtt.org/) Python packages based on "
+    "the [JSon](https://www.json.org) [ETSI](https://www.etsi.org/committee/its) specification transcription.",
+    license="MIT",
+    platforms="LINUX",
+    install_requires=[],
+)
