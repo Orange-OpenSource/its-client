724fb08dabeffc9012ecb519f3087be425e24307
Nicolas Buffon <nicolas.buffon@orange.com>
2023-11-15 15:16:05 +0100

    rust: add etsi module
    This module describes the ETSI messages which are intended to be
    exchanged
    It also provides the functions to converts data from SI units to ETSI
    fancy ones
    
    Signed-off-by: Nicolas Buffon <nicolas.buffon@orange.com>

---
+++ rust/src/exchange/etsi.rs
@@ -0,0 +1,9 @@
+// Software Name: its-client
+// SPDX-FileCopyrightText: Copyright (c) 2016-2023 Orange
+// SPDX-License-Identifier: MIT License
+//
+// This software is distributed under the MIT license, see LICENSE.txt file for more details.
+//
+// Author: Nicolas BUFFON <nicolas.buffon@orange.com> et al.
+// Software description: This Intelligent Transportation Systems (ITS) [MQTT](https://mqtt.org/) client based on the [JSon](https://www.json.org) [ETSI](https://www.etsi.org/committee/its) specification transcription provides a ready to connect project for the mobility (connected and autonomous vehicles, road side units, vulnerable road users,...).
+
