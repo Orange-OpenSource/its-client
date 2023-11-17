f759ad47021461d9431d2d928d5186c811bdf83d
Nicolas Buffon <nicolas.buffon@orange.com>
2023-11-17 14:56:26 +0100

    rust: rust: add Content trait in message mod
    Signed-off-by: Nicolas Buffon <nicolas.buffon@orange.com>

--- rust/src/exchange/message/content.rs
+++ rust/src/exchange/message/content.rs
@@ -1,5 +1,5 @@
 // Software Name: its-client
-// SPDX-FileCopyrightText: Copyright (c) 2016-2023 Orange
+// SPDX-FileCopyrightText: Copyright (c) 2016-2022 Orange
 // SPDX-License-Identifier: MIT License
 //
 // This software is distributed under the MIT license, see LICENSE.txt file for more details.
@@ -7,10 +7,16 @@
 // Author: Nicolas BUFFON <nicolas.buffon@orange.com> et al.
 // Software description: This Intelligent Transportation Systems (ITS) [MQTT](https://mqtt.org/) client based on the [JSon](https://www.json.org) [ETSI](https://www.etsi.org/committee/its) specification transcription provides a ready to connect project for the mobility (connected and autonomous vehicles, road side units, vulnerable road users,...).
 
-pub mod content;
-pub mod content_error;
+use crate::exchange::message::content_error::ContentError;
+use crate::exchange::mortal::Mortal;
+use crate::mobility::mobile::Mobile;
 
-use serde::{Deserialize, Serialize};
+pub trait Content {
+    fn get_type(&self) -> &str;
 
-#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
-pub enum Message {}
+    fn appropriate(&mut self);
+
+    fn as_mobile(&self) -> Result<&dyn Mobile, ContentError>;
+
+    fn as_mortal(&self) -> Result<&dyn Mortal, ContentError>;
+}
