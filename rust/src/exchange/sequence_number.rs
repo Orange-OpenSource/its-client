bc65000b0e515536983d198f36f5f2189e801b50
Nicolas Buffon <nicolas.buffon@orange.com>
2023-11-23 15:59:58 +0100

    rust: SequenceNumber struct
    Signed-off-by: Nicolas Buffon <nicolas.buffon@orange.com>

---
+++ rust/src/exchange/sequence_number.rs
@@ -0,0 +1,66 @@
+// Software Name: its-client
+// SPDX-FileCopyrightText: Copyright (c) 2016-2023 Orange
+// SPDX-License-Identifier: MIT License
+//
+// This software is distributed under the MIT license, see LICENSE.txt file for more details.
+//
+// Author: Nicolas BUFFON <nicolas.buffon@orange.com> et al.
+// Software description: This Intelligent Transportation Systems (ITS) [MQTT](https://mqtt.org/) client based on the [JSon](https://www.json.org) [ETSI](https://www.etsi.org/committee/its) specification transcription provides a ready to connect project for the mobility (connected and autonomous vehicles, road side units, vulnerable road users,...).
+
+use std::fmt::Formatter;
+use std::sync::Mutex;
+
+#[derive(Default)]
+pub struct SequenceNumber {
+    current: Mutex<u128>,
+    max: u128,
+}
+
+impl SequenceNumber {
+    #[allow(dead_code)]
+    pub fn new(max: u128) -> Self {
+        Self {
+            current: Mutex::new(0),
+            max,
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn get_next(&mut self) -> u128 {
+        let mut write_lock = self.current.lock().unwrap();
+        *write_lock = (*write_lock + 1) % self.max;
+        *write_lock
+    }
+}
+
+impl std::fmt::Display for SequenceNumber {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", *self.current.lock().unwrap())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::exchange::sequence_number::SequenceNumber;
+
+    #[test]
+    fn next_sequence_number() {
+        let mut sequence_number = SequenceNumber::new(u16::MAX.into());
+
+        let next = sequence_number.get_next();
+
+        assert_eq!(next, 1);
+    }
+
+    #[test]
+    fn next_sequence_number_modulo() {
+        let mut sequence_number = SequenceNumber::new(u16::MAX.into());
+        for _i in 0..65534 {
+            sequence_number.get_next();
+        }
+
+        let next = sequence_number.get_next();
+
+        assert_eq!(next, 0);
+    }
+}
