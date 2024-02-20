192f513aa5f0bc2b0bb28dea4d5987417b73123a
Nicolas Buffon <nicolas.buffon@orange.com>
2024-02-20 12:27:54 +0100

    rust: update clippy's clause name
    Note that this also needs a docstring to explain why implementation
    differ
    
    Signed-off-by: Nicolas Buffon <nicolas.buffon@orange.com>

--- rust/src/mobility/quadtree/quadkey.rs
+++ rust/src/mobility/quadtree/quadkey.rs
@@ -139,7 +139,7 @@ impl Ord for Quadkey {
     }
 }
 
-#[allow(clippy::incorrect_partial_ord_impl_on_ord_type)]
+#[allow(clippy::non_canonical_partial_ord_impl)]
 impl PartialOrd for Quadkey {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         let matching = self
