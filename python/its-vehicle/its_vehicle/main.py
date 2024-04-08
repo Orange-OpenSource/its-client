dbc4bcc6d0a1c571db9be124a2835d0d0f7d733d
Yann E. MORIN <yann.morin@orange.com>
2024-04-08 13:45:54 +0200

    python/its-vehicle: introduce a tracker
    The tracker is responsible for tracking events, like when a message is
    sent, received, etc... For now, does nothing except hand out an ID.
    
    Signed-off-by: Yann E. MORIN <yann.morin@orange.com>

--- python/its-vehicle/its_vehicle/main.py
+++ python/its-vehicle/its_vehicle/main.py
@@ -12,6 +12,7 @@ import sys
 from . import client
 from . import gpsd
 from . import mqtt
+from . import tracking
 
 
 CFG = "/etc/its/vehicle.cfg"
@@ -102,6 +103,9 @@ def main():
     else:
         mqtt_mirror = None
 
+    tracker = tracking.Tracking(cfg=cfg["tracking"], gpsd=gpsd)
+    tracker.start()
+
     its_client = client.ITSClient(
         cfg=cfg["general"],
         gpsd=gnss,
@@ -131,6 +135,7 @@ def main():
 
     logging.debug("Will stop...")
     its_client.stop(wait=True)
+    tracker.stop()
     # Stop main MQTT client before the mirror one, so that we don't get
     # messages from the main one that we would then try to publish on
     # the mirror one that we just stopped.
