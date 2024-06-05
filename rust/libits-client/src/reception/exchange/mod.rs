95cc184b3c6e0951a983dbfb409f67d229adce75
Nicolas Buffon <nicolas.buffon@orange.com>
2024-06-05 07:13:19 +0200

    rust: add mobile id in PathElement
    Signed-off-by: Nicolas Buffon <nicolas.buffon@orange.com>

--- rust/libits-client/src/reception/exchange/mod.rs
+++ rust/libits-client/src/reception/exchange/mod.rs
@@ -42,6 +42,7 @@ pub struct Exchange {
 #[serde_with::skip_serializing_none]
 #[derive(Debug, Clone, Hash, PartialEq, Serialize, Deserialize)]
 pub struct PathElement {
+    pub mobile_id: u32,
     pub position: ReferencePosition,
     pub message_type: String,
 }
