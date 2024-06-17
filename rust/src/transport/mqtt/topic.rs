4fd38adb1fe52ad90bb0e4a9dc800cb4c147f567
Nicolas Buffon <nicolas.buffon@orange.com>
2024-06-17 17:10:21 +0200

    rust: update source files header
    Using new header version from Open Source Orange [internal page][1]
    
    [1]: https://infoportal-opensource.innov.intraorange/en/copyright-and-license-notices/
    
    Signed-off-by: Nicolas Buffon <nicolas.buffon@orange.com>

--- rust/src/transport/mqtt/topic.rs
+++ rust/src/transport/mqtt/topic.rs
@@ -1,11 +1,14 @@
-// Software Name: its-client
-// SPDX-FileCopyrightText: Copyright (c) 2016-2023 Orange
-// SPDX-License-Identifier: MIT License
-//
-// This software is distributed under the MIT license, see LICENSE.txt file for more details.
-//
-// Author: Nicolas Buffon <frederic.gardes@orange.com> et al.
-// Software description: This Intelligent Transportation Systems (ITS) [MQTT](https://mqtt.org/) client based on the [JSon](https://www.json.org) [ETSI](https://www.etsi.org/committee/its) specification transcription provides a ready to connect project for the mobility (connected and autonomous vehicles, road side units, vulnerable road users,...).
+/*
+ * Software Name : libits
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license,
+ * see the "LICENSE.txt" file for more details or https://opensource.org/license/MIT/
+ *
+ * Authors: see CONTRIBUTORS.md
+ * Software description: This Intelligent Transportation Systems (ITS) [MQTT](https://mqtt.org/) library based on the [JSon](https://www.json.org) [ETSI](https://www.etsi.org/committee/its) specification transcription provides a ready to connect project for the mobility (connected and autonomous vehicles, road side units, vulnerable road users,...).
+ */
 
 use std::fmt::{Debug, Display};
 use std::hash::Hash;
