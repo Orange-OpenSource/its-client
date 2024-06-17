06f38a26d194dbb56e287b4e79e8dc570e7c73d5
Nicolas Buffon <nicolas.buffon@orange.com>
2024-06-17 17:10:21 +0200

    rust: update source files header
    Using new header version from Open Source Orange [internal page][1]
    
    [1]: https://infoportal-opensource.innov.intraorange/en/copyright-and-license-notices/
    
    Signed-off-by: Nicolas Buffon <nicolas.buffon@orange.com>

--- rust/src/mobility/quadtree.rs
+++ rust/src/mobility/quadtree.rs
@@ -1,3 +1,15 @@
+/*
+ * Software Name : libits
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license,
+ * see the "LICENSE.txt" file for more details or https://opensource.org/license/MIT/
+ *
+ * Authors: see CONTRIBUTORS.md
+ * Software description: This Intelligent Transportation Systems (ITS) [MQTT](https://mqtt.org/) library based on the [JSon](https://www.json.org) [ETSI](https://www.etsi.org/committee/its) specification transcription provides a ready to connect project for the mobility (connected and autonomous vehicles, road side units, vulnerable road users,...).
+ */
+
 use crate::mobility::quadtree::quadkey::Quadkey;
 use std::f64::consts::PI;
 
