2cf077019bafc16a36f7db52a4acc7b8bcdd4633
Mathieu Lefebvre <mathieu1.lefebvre@orange.com>
2024-07-09 15:56:29 +0200

    java: add copyright and license details

--- java/iot3/mobility/src/main/java/com/orange/iot3mobility/IoT3MobilityCallback.java
+++ java/iot3/mobility/src/main/java/com/orange/iot3mobility/IoT3MobilityCallback.java
@@ -1,3 +1,10 @@
+/*
+ Copyright 2016-2024 Orange
+
+ This software is distributed under the MIT license, see LICENSE.txt file for more details.
+
+ @author Mathieu LEFEBVRE <mathieu1.lefebvre@orange.com>
+ */
 package com.orange.iot3mobility;
 
 public interface IoT3MobilityCallback {
