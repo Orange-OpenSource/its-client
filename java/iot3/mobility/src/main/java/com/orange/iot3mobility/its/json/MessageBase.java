02e5b81fd0b926c757eff5efc468cd7384520c8d
Mathieu Lefebvre <mathieu1.lefebvre@orange.com>
2024-08-12 16:40:51 +0200

    java/iot3mobility: clean MessageBase class

--- java/iot3/mobility/src/main/java/com/orange/iot3mobility/its/json/MessageBase.java
+++ java/iot3/mobility/src/main/java/com/orange/iot3mobility/its/json/MessageBase.java
@@ -7,13 +7,7 @@
  */
 package com.orange.iot3mobility.its.json;
 
-/**
- * Created by Mathieu Lefebvre on 22/02/18.
- * Feel free to improve it
- */
-
 import com.orange.iot3mobility.TrueTime;
-import com.orange.iot3mobility.Utils;
 
 /**
  * Common base of all Orange ITS JSON messages.
@@ -21,77 +15,28 @@ import com.orange.iot3mobility.Utils;
 public abstract class MessageBase {
 
     private final String type;
-    private final String context;
     private final String origin;
     private final String version;
-    private String sourceUuid;
+    private final String sourceUuid;
     private final String destinationUuid;
     private long timestamp;
-    private String messageId;
-    private final String extra = "";
-    private final String signature;
 
-    public MessageBase(String type, String context, String origin, String version,
-                       String sourceUuid, String destinationUuid, long timestamp,
-                       String messageUuid, String signature) {
-        this.type = type;
-        this.context = context;
-        this.origin = origin;
-        this.version = version;
-        this.sourceUuid = sourceUuid;
-        this.destinationUuid = destinationUuid;
-        this.timestamp = timestamp;
-        this.messageId = sourceUuid+"/"+messageUuid+"/"+timestamp;
-        this.signature = signature;
-    }
-
-    public MessageBase(String type,
+    protected MessageBase(String type,
                        String origin,
                        String version,
                        String sourceUuid,
                        String destinationUuid,
                        long timestamp) {
-        this(type, JsonValue.Context.UNDEFINED.value(), origin, version, sourceUuid, destinationUuid, timestamp, "");
-    }
-
-    public MessageBase(String type,
-                       String context,
-                       String origin,
-                       String version,
-                       String sourceUuid,
-                       long timestamp,
-                       String signature) {
-        this(type, context, origin, version, sourceUuid, "", timestamp, signature);
-    }
-
-    public MessageBase(String type,
-                       String context,
-                       String origin,
-                       String version,
-                       String sourceUuid,
-                       String destinationUuid,
-                       long timestamp,
-                       String signature) {
-        this(type, context, origin, version, sourceUuid, destinationUuid, timestamp, Utils.getRandomUuid(), signature);
-    }
-
-    public MessageBase(String type,
-                       String context,
-                       String origin,
-                       String version,
-                       String sourceUuid,
-                       long timestamp,
-                       String messageUuid,
-                       String signature) {
-        this(type, context, origin, version, sourceUuid, "", timestamp, messageUuid, signature);
+        this.type = type;
+        this.origin = origin;
+        this.version = version;
+        this.sourceUuid = sourceUuid;
+        this.destinationUuid = destinationUuid;
+        this.timestamp = timestamp;
     }
 
     public String getType() {
         return type;
-    }
-
-    public String getContext() {
-        return context;
     }
 
     public String getOrigin() {
@@ -102,22 +47,8 @@ public abstract class MessageBase {
         return version;
     }
 
-    public int getVersionInt() {
-        String tmpVersion = version.replace(".", "");
-        return Integer.parseInt(tmpVersion);
-    }
-
-    public static int getVersionInt(String version) {
-        String tmpVersion = version.replace(".", "");
-        return Integer.parseInt(tmpVersion);
-    }
-
     public String getSourceUuid() {
         return sourceUuid;
-    }
-
-    public void updateSourceUuid() {
-        //sourceUuid = SELF.getSourceUUID();
     }
 
     public String getDestinationUuid() {
@@ -130,18 +61,6 @@ public abstract class MessageBase {
 
     public void updateTimestamp() {
         timestamp = TrueTime.getAccurateTime();
-    }
-
-    public String getMessageId() {
-        return messageId;
-    }
-
-    public String getExtra() {
-        return extra;
-    }
-
-    public String getSignature() {
-        return signature;
     }
 
 }
