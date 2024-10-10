36e5c5d938b17002ae7a020ec958ff6726b96059
tigroo <frederic.gardes@orange.com>
2024-10-10 06:08:12 +0000

    init

---
+++ trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"libits":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"libits/client/configuration/configuration_error/enum.ConfigurationError.html\" title=\"enum libits::client::configuration::configuration_error::ConfigurationError\">ConfigurationError</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
