Orange-OpenSource/its-client

commit c482cb16774f0e0dfdcc79af03935b23a64ff7da

python/vehicle: use iot3.core.mqtt
Signed-off-by: Yann E. MORIN <yann.morin@orange.com>

python/its-vehicle/its_vehicle/client.py
@@ -8,9 +8,8 @@ import json
 import linuxfd
 import logging
 import threading
-from paho.mqtt.client import MQTTMessage
 from .gpsd import GNSSProvider
-from .mqtt import MqttClient
+from iot3.core.mqtt import MqttClient
 from .roi import RegionOfInterest
 from .its.cam import CooperativeAwarenessMessage as CAM
 
@@ -147,7 +146,7 @@ class ITSClient:
                         ),
                     ),
                 )
-            self.mqtt_main.subscribe_replace(list(roi_topics))
+            self.mqtt_main.subscribe_replace(topics=list(roi_topics))
 
             msg = self.ITSMessage(
                 uuid=self.cfg["instance-id"],
@@ -155,7 +154,7 @@ class ITSClient:
             )
             topic = self.pub_topic_root + quadkey.to_str("/")
             msg_json = msg.to_json()
-            self.mqtt_main.publish(topic, msg_json)
+            self.mqtt_main.publish(topic=topic, payload=msg_json)
             if self.mqtt_mirror and not self.cfg["mirror-self"]:
                 # Use the sub-prefix, to simulate the message as coming
                 # from the main broker as if we had subscribed to it.
@@ -163,21 +162,27 @@ class ITSClient:
                     self.cfg["topic-sub-prefix"]
                     + topic[len(self.cfg["topic-pub-prefix"]) :]
                 )
-                self.mqtt_mirror.publish(mirror_topic, msg_json)
+                self.mqtt_mirror.publish(topic=mirror_topic, payload=msg_json)
 
         # Out of the loop, cleanup and close
         self.mqtt_main.unsubscribe_all()
-        self.mqtt_main.set_msg_cb(None)
         timer.close()
 
-    def msg_cb(self, message: MQTTMessage):
+    def msg_cb(
+        self,
+        *_args,
+        topic: str,
+        payload: bytes,
+        **_kwargs,
+    ):
         logging.debug(
             "received mesage on %s: %s",
-            message.topic[:16] + "..." if len(message.topic) > 16 else "",
-            message.payload[:16] + "..." if len(message.payload) > 16 else "",
+            topic[:16] + "..." if len(topic) > 16 else "",
+            payload[:16].decode(errors="backslashreplace")
+            + ("..." if len(payload) > 16 else ""),
         )
         try:
-            payload = json.loads(message.payload)
+            payload_d = json.loads(payload)
         except json.decoder.JSONDecodeError:
             # Not JSON, not sure what to do with that...
             return
@@ -185,10 +190,10 @@ class ITSClient:
             return
         try:
             if (
-                payload.get("source_uuid", None) != self.cfg["instance-id"]
+                payload_d.get("source_uuid", None) != self.cfg["instance-id"]
                 or self.cfg["mirror-self"]
             ):
-                self.mqtt_mirror.publish(message.topic, message.payload)
+                self.mqtt_mirror.publish(topic, payload)
         except:
             # Payload does not have expected fields, or is not a dict;
             # ignore this invalid message

python/its-vehicle/its_vehicle/main.py
@@ -5,13 +5,13 @@
 
 import argparse
 import configparser
+import iot3.core.mqtt
 import logging
 import os
 import signal
 import sys
 from . import client
 from . import gpsd
-from . import mqtt
 
 
 CFG = "/etc/its/vehicle.cfg"
@@ -95,11 +95,39 @@ def main():
     def _msg_cb(*args, **kwargs):
         its_client.msg_cb(*args, **kwargs)
 
-    mqtt_main = mqtt.MqttClient(cfg=cfg["broker.main"])
-    mqtt_main.set_msg_cb(_msg_cb)
+    if "host" in cfg["broker.main"]:
+        conn_opts = {
+            "host": cfg["broker.main"]["host"],
+            "port": int(cfg["broker.main"]["port"]),
+        }
+    else:
+        conn_opts = {
+            "socket_path": cfg["broker.main"]["socket-path"],
+        }
+    mqtt_main = iot3.core.mqtt.MqttClient(
+        client_id=cfg["broker.main"]["client-id"],
+        username=cfg["broker.main"]["username"],
+        password=cfg["broker.main"]["password"],
+        **conn_opts,
+        msg_cb=_msg_cb,
+    )
 
     if "host" in cfg["broker.mirror"] or "socket-path" in cfg["broker.mirror"]:
-        mqtt_mirror = mqtt.MqttClient(cfg=cfg["broker.mirror"])
+        if "host" in cfg["broker.mirror"]:
+            conn_opts = {
+                "host": cfg["broker.mirror"]["host"],
+                "port": int(cfg["broker.mirror"]["port"]),
+            }
+        else:
+            conn_opts = {
+                "socket_path": cfg["broker.mirror"]["socket-path"],
+            }
+        mqtt_mirror = iot3.core.mqtt.MqttClient(
+            client_id=cfg["broker.mirror"]["client-id"],
+            username=cfg["broker.mirror"]["username"],
+            password=cfg["broker.mirror"]["password"],
+            **conn_opts,
+        )
     else:
         mqtt_mirror = None
 

python/its-vehicle/setup.py
@@ -36,6 +36,7 @@ setup(
     license="MIT",
     platforms="LINUX",
     install_requires=[
+        "iot3",
         "its-quadkeys",
         "linuxfd==1.5",
         "paho-mqtt >=1.6.1, !=2.0.*",

commit 37730406a4c6aa43a804e21e443909beea54606f

python/vehicle: fix propagating messages to local broker
Commit c482cb16774f (python/vehicle: use iot3.core.mqtt) introduced a
regression in the propagation of messages from the remote broker to
the local one.

The API for the IoT3 Core SDK extensively uses keywords rather than
positional parameters, and although this was properly accounted when
publishing messages on the remote broker, this was overlooked when
publishing messages on the local broker.

Such an issue would have raised an exception, and we would have caught
it quite easily. Alas, in an attempt at being robust when decoding the
received messages, we decided for a blanket coverage of all exceptions,
no matter what. This indeed hid the TypeError exception that is raised
when improperly calling the SDK...

So, now, we could just catch KeyError when looking up the source_uuid
field, but that would assume the message was decoded as a dict, which we
have no guarantee it was. If that were not the case, i.e.e the message
is not an ITS message, and contains any other JSON object that is not a
dictionary, that would _also_ raise TypeError, so we would have to catch
it anyway to ignore it. And we would not have been able to notice the
TypeError raised by improperly calling the SDK either anyway...

Sigh...

For now, just fix calling the SDK; we'll handle the rest later (Famous
Last Words™)...

Fixes: #203

Signed-off-by: Yann E. MORIN <yann.morin@orange.com>

python/its-vehicle/its_vehicle/client.py
@@ -193,7 +193,7 @@ class ITSClient:
                 payload_d.get("source_uuid", None) != self.cfg["instance-id"]
                 or self.cfg["mirror-self"]
             ):
-                self.mqtt_mirror.publish(topic, payload)
+                self.mqtt_mirror.publish(topic=topic, payload=payload)
         except:
             # Payload does not have expected fields, or is not a dict;
             # ignore this invalid message