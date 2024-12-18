2c07c5c619d00b67ad6ad0dc5d9ad820191efb3b
tigroo <frederic.gardes@orange.com>
2024-12-18 15:51:22 +0000

    init

---
+++ crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["libits"];
+//{"start":21,"fragment_lengths":[8]}
---
+++ trait.impl/opentelemetry/propagation/trait.Extractor.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["libits",[["impl&lt;T: <a class=\"trait\" href=\"libits/transport/mqtt/topic/trait.Topic.html\" title=\"trait libits::transport::mqtt::topic::Topic\">Topic</a>, P: <a class=\"trait\" href=\"libits/transport/payload/trait.Payload.html\" title=\"trait libits::transport::payload::Payload\">Payload</a>&gt; Extractor for <a class=\"struct\" href=\"libits/transport/packet/struct.Packet.html\" title=\"struct libits::transport::packet::Packet\">Packet</a>&lt;T, P&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[468]}
