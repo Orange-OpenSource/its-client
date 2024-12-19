a8c904c64fdca110f3f45f388779cf5fc66d08e9
François Suc <francois.suc@orange.com>
2024-12-19 15:22:41 +0100

    swift: add MQTT client
    Add a protocol MQTTClient with an implementation based on MQTT NIO
    
    Signed-off-by: François Suc <francois.suc@orange.com>

---
+++ swift/ITSClient/Sources/ITSCore/MQTT/MQTTClientConfiguration.swift
@@ -0,0 +1,38 @@
+/*
+ * Software Name : ITSClient
+ * SPDX-FileCopyrightText: Copyright (c) Orange SA
+ * SPDX-License-Identifier: MIT
+ *
+ * This software is distributed under the MIT license,
+ * see the "LICENSE.txt" file for more details or https://opensource.org/license/MIT/
+ *
+ * Software description: Swift ITS client.
+ */
+
+struct MQTTClientConfiguration {
+    let host: String
+    let port: Int
+    let clientIdentifier: String
+    let userName: String?
+    let password: String?
+    let useSSL: Bool
+    let useWebSockets: Bool
+    
+    init(
+        host: String,
+        port: Int,
+        clientIdentifier: String,
+        userName: String? = nil,
+        password: String? = nil,
+        useSSL: Bool,
+        useWebSockets: Bool = false
+    ) {
+        self.host = host
+        self.port = port
+        self.clientIdentifier = clientIdentifier
+        self.userName = userName
+        self.password = password
+        self.useSSL = useSSL
+        self.useWebSockets = useWebSockets
+    }
+}
