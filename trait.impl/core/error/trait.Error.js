3219b0ab6b1d59ca062806cfc32a7acf5bed1b41
tigroo <frederic.gardes@orange.com>
2025-02-27 15:58:47 +0000

    init

---
+++ trait.impl/core/error/trait.Error.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["libits",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"libits/client/configuration/configuration_error/enum.ConfigurationError.html\" title=\"enum libits::client::configuration::configuration_error::ConfigurationError\">ConfigurationError</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[372]}
