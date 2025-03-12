Orange-OpenSource/its-client

commit 730a40a9e177c8df2ff4286f2a7a9e5136808bb1

python/iqm: apply configurable filters on receive and publish
Signed-off-by: Yann E. MORIN <yann.morin@orange.com>

python/its-interqueuemanager/src/its_iqm/filters.py
@@ -1,0 +1,147 @@
+# Software Name: its-interqueuemanager
+# SPDX-FileCopyrightText: Copyright (c) 2025 Orange
+# SPDX-License-Identifier: MIT
+# Author: Yann E. MORIN <yann.morin@orange.com>
+
+import itertools
+import json
+import logging
+import re
+
+
+class Filter:
+    def __init__(
+        self,
+        *,
+        name: str,
+        filter_cfg: dict,
+        instance_id: str,
+        prefix: str,
+        suffix: str,
+        queues: dict,
+    ):
+        self.name = name
+
+        placeholders = {
+            "instance-id": instance_id,
+            "prefix": prefix,
+            "suffix": suffix,
+        }
+        placeholders.update(queues)
+
+        self._patterns = None
+        for filter_type, filter_kind in itertools.product(
+            ["in", "out"],
+            ["prefix", "regex"],
+        ):
+            try:
+                patterns = filter_cfg[filter_type + "_" + filter_kind]
+            except KeyError:
+                continue
+
+            if self._patterns is not None:
+                raise ValueError(f"Filter {name} defines multiple patterns")
+            patterns = list(filter(None, patterns.split("\n")))
+            for ph in placeholders:
+                patterns = list(
+                    map(
+                        lambda s: s.replace("{{" + ph + "}}", placeholders[ph]),
+                        patterns,
+                    ),
+                )
+            if filter_kind == "regex":
+                patterns = list(map(re.compile, patterns))
+
+            self._patterns = patterns
+            self._type = filter_type
+            self._kind = filter_kind
+
+        if patterns is None:
+            raise ValueError(f"Filter '{name}' does not define patterns")
+
+        self._drop = "drop" in filter_cfg
+
+        match filter_cfg.get("retain"):
+            case None:
+                self._retain = None
+            case "True" | "true" | "False" | "false" as b:
+                self._retain = b in ["True", "true"]
+            case str(s) if re.match("^\d+$", s):
+                self._retain = int(s)
+            case str(s) if s.startswith("json:") and re.match(".* \d+$", s):
+                path, fallback = s[5:].split(" ")
+                self._retain = {
+                    "path": path.split("."),
+                    "fallback": int(fallback),
+                }
+            case str(s) if s.startswith("json:"):
+                self._retain = {
+                    "path": s[5:].split("."),
+                    "fallback": None,
+                }
+            case _v:
+                raise ValueError(f"Unable to parse retain value '{_v}'")
+
+        logging.debug(f"Created new filter {name}:")
+        logging.debug(f"  - type: {self._type}")
+        logging.debug(f"  - kind: {self._kind}")
+        logging.debug(f"  - patterns: {self._patterns}")
+        logging.debug(f"  - drop: {self._drop}")
+        logging.debug(f"  - retain: {self._retain}")
+
+    @property
+    def type(self):
+        return self._type
+
+    def apply(
+        self,
+        *,
+        topic: str,
+        payload: bytes,
+        retain: bool | int,
+    ):
+        for pattern in self._patterns:
+            if self._kind == "prefix" and topic.startswith(pattern):
+                break
+            if self._kind == "regex" and pattern.match(topic):
+                break
+        else:
+            logging.debug(f"{self.name}[{self._type}]: no match for {topic}")
+            return topic, payload, retain
+
+        logging.debug(
+            f"{self.name}[{self._type}]: match for {topic} with {self._kind} {pattern}"
+        )
+
+        if self._drop:
+            logging.debug(f"{self.name}:   - dropped")
+            return None, None, None
+
+        match self._retain:
+            case None:
+                logging.debug("{self.name}:   - retain: None")
+            case bool(b):
+                logging.debug(f"{self.name}:   - retain: {b}")
+                retain = b
+            case int(i):
+                logging.debug(f"{self.name}:   - retain: {i}")
+                retain = i
+            case {"path": path, "fallback": fallback}:
+                logging.debug(f"{self.name}:   - retain: {path}")
+                try:
+                    data = json.loads(payload)
+                    for p in path:
+                        data = data[p]
+                except (json.decoder.JSONDecodeError, KeyError, TypeError):
+                    if fallback is not None:
+                        logging.debug(f"{self.name}:     - using fallback {fallback}")
+                        retain = fallback
+                else:
+                    retain = data
+
+        logging.debug(f"{self.name}: Result of filter:")
+        logging.debug(f"{self.name}:   - topic: {topic}")
+        logging.debug(f"{self.name}:   - payload: {payload}")
+        logging.debug(f"{self.name}:   - retain: {retain}")
+
+        return topic, payload, retain

python/its-interqueuemanager/its-iqm.cfg
@@ -27,6 +27,41 @@ socket-path = /run/mosquitto/mqtt.socket
 # Name of the local inter-queue; default: interQueue
 #interqueue = interQueue
 
+# Sections which name starts with 'filtering.' (with the dot) followed
+# by an arbitrary identifier, define filtering rules and actions.
+#[filter.NAME]
+# The prefix or regexp to match topic against; only one kind of match can
+# be specified for a given filter (either by prefix or by regexp), but
+# multiple such match can be specified with a multi-line value.
+# Placeholders {{XXX}} will be replaced before attempting a match:
+#  - {{instance-id}}: replaced by general.instance-id
+#  - {{prefix}}: if general.prefix is set, this placeholder is replaced
+#    with it, with a trailing '/' appended; otherwise, an empty string;
+#  - {{suffix}}: replaced by general.suffix if set, an empty string otherwise;
+#  - {{inQueue}}, {{outQueue}}, {{interQueue}}: placeholders matching the
+#    inQueue, outQueue, and interQueue, respectively.
+#in_prefix =
+#   prefix-str-1
+#   prefix-str-2
+#in_regex = regex-1
+#   regex-2
+#out_prefix = prefix-str
+#out_regex = regex
+# What to do with the message:
+# - drop the message if the topic matches:
+#drop
+# - set the message as retained or as not retained:
+#retain = True / False
+# - set the message as retained and set property Message Expiry Interval
+#   to the specified value (in seconds):
+#retain = INT
+# - set the message as retained and set property Message Expiry Interval
+#   to the value from the JSON payload object at the specified dotted
+#   path; if the path does not exist or is not an integer, use the
+#   fallback value if specified, otherwise same as if retain was not
+#   specified:
+#retain = json:DOTTED.PATH.IN.JSON.PAYLOAD [FALLBACK_INT]
+
 [authority]
 # Type of central authority, one of: file, http, mqtt
 type = file

python/its-interqueuemanager/src/its_iqm/iqm.py
@@ -10,6 +10,7 @@ import iot3.core.otel
 import logging
 import time
 from . import authority
+from . import filters
 
 
 class IQM:
@@ -41,6 +42,12 @@ class IQM:
         # it every time
         self.outqueue = outqueue
 
+        queues = {
+            "inQueue": inqueue,
+            "outQueue": outqueue,
+            "interQueue": interqueue,
+        }
+
         if cfg["telemetry"]["endpoint"]:
             self.otel = iot3.core.otel.Otel(
                 service_name="its-interqueuemanager",
@@ -57,12 +64,29 @@ class IQM:
             self.otel = None
             self.span_cb = iot3.core.otel.Otel.noexport_span
 
+        self.filters = {
+            "in": list(),
+            "out": list(),
+        }
+        for sect_name in [s for s in cfg if s.startswith("filter.")]:
+            filter_name = sect_name[7:]
+            new_filter = filters.Filter(
+                name=filter_name,
+                filter_cfg=cfg[sect_name],
+                instance_id=self.instance_id,
+                prefix=prefix,
+                suffix=suffix,
+                queues=queues,
+            )
+            self.filters[new_filter.type].append(new_filter)
+
         logging.info("create local qm")
 
         qm_data = {
             "copy_qm": None,
             "copy_from": inqueue,
             "copy_to": [outqueue, interqueue],
+            "filters": self.filters,
         }
 
         self.local_qm = iot3.core.mqtt.MqttClient(
@@ -161,6 +185,7 @@ class IQM:
                 "copy_qm": self.local_qm,
                 "copy_from": interqueue,
                 "copy_to": [self.outqueue],
+                "filters": self.filters,
             }
             creds = {
                 "username": None,
@@ -190,10 +215,32 @@ class IQM:
         payload: bytes,
         **_kwargs,
     ):
-        mqtt_cli = data["copy_qm"] or self.local_qm
-        for cp_to in data["copy_to"]:
-            new_topic = cp_to + topic[len(data["copy_from"]) :]
-            mqtt_cli.publish(
-                topic=new_topic,
+        retain = False
+        for in_filter in data["filters"]["in"]:
+            topic, payload, retain = in_filter.apply(
+                topic=topic,
                 payload=payload,
+                retain=retain,
             )
+            if topic is None:
+                break
+        else:
+            mqtt_cli = data["copy_qm"] or self.local_qm
+            for cp_to in data["copy_to"]:
+                new_topic = cp_to + topic[len(data["copy_from"]) :]
+                new_payload = payload
+                new_retain = retain
+                for out_filter in data["filters"]["out"]:
+                    new_topic, new_payload, new_retain = out_filter.apply(
+                        topic=new_topic,
+                        payload=new_payload,
+                        retain=new_retain,
+                    )
+                    if new_topic is None:
+                        break
+                else:
+                    mqtt_cli.publish(
+                        topic=new_topic,
+                        payload=new_payload,
+                        retain=new_retain,
+                    )

python/its-interqueuemanager/src/its_iqm/main.py
@@ -61,7 +61,7 @@ def main():
     )
 
     logging.info(f"loading config file {args.config}...")
-    cfg = configparser.ConfigParser()
+    cfg = configparser.ConfigParser(allow_no_value=True)
     with open(args.config) as f:
         cfg.read_file(f)
 

commit 35cde9864b74ad4c8dd92c301794909b478dbefe

python/iqm: fix error when filter defines no pattern
The `patterns` variable is only defined when there is actualyl a
pattern, so we can't test its value to detect there was no pattern
defined.

Instead, we must use the object's attribute `_patterns`, which is
assigned before the loop.

Closes: #313
Fixes: 730a40a9e177 (python/iqm: apply configurable filters on [...])

Signed-off-by: Yann E. MORIN <yann.morin@orange.com>

python/its-interqueuemanager/src/its_iqm/filters.py
@@ -56,7 +56,7 @@ class Filter:
             self._type = filter_type
             self._kind = filter_kind
 
-        if patterns is None:
+        if self._patterns is None:
             raise ValueError(f"Filter '{name}' does not define patterns")
 
         self._drop = "drop" in filter_cfg