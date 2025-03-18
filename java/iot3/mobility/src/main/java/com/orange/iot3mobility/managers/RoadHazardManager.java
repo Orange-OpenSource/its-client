7c68e2b2d076985e14476ab898c1e3ccf1c986d5
Mathieu LEFEBVRE <mathieu1.lefebvre@orange.com>
2025-03-18 09:56:29 +0100

    java/iot3mobility: add getter returning a read-only list of road hazards to the RoadHazardManager
    Signed-off-by: Mathieu LEFEBVRE <mathieu1.lefebvre@orange.com>

--- java/iot3/mobility/src/main/java/com/orange/iot3mobility/managers/RoadHazardManager.java
+++ java/iot3/mobility/src/main/java/com/orange/iot3mobility/managers/RoadHazardManager.java
@@ -16,9 +16,7 @@ import com.orange.iot3mobility.roadobjects.RoadHazard;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -119,6 +117,15 @@ public class RoadHazardManager {
             scheduler.scheduleWithFixedDelay(RoadHazardManager::checkAndRemoveExpiredObjects,
                     1, 1, TimeUnit.SECONDS);
         }
+    }
+
+    /**
+     * Retrieve a read-only list of the Road Hazards in the vicinity.
+     *
+     * @return the read-only list of {@link com.orange.iot3mobility.roadobjects.RoadHazard} objects
+     */
+    public static List<RoadHazard> getRoadHazards() {
+        return Collections.unmodifiableList(ROAD_HAZARDS);
     }
 
 }
