e3bd64e7cf16c90dcfdadb32f01203f46e9ba26b
tigroo <frederic.gardes@orange.com>
2025-03-20 08:24:06 +0000

    init

---
+++ trait.impl/core/cmp/trait.Eq.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["libits",[["impl&lt;T, P&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"libits/transport/packet/struct.Packet.html\" title=\"struct libits::transport::packet::Packet\">Packet</a>&lt;T, P&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"libits/transport/mqtt/topic/trait.Topic.html\" title=\"trait libits::transport::mqtt::topic::Topic\">Topic</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a>,\n    P: <a class=\"trait\" href=\"libits/transport/payload/trait.Payload.html\" title=\"trait libits::transport::payload::Payload\">Payload</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a>,</div>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[873]}
---
+++ trait.impl/core/marker/trait.StructuralPartialEq.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["libits",[["impl&lt;T, P&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"struct\" href=\"libits/transport/packet/struct.Packet.html\" title=\"struct libits::transport::packet::Packet\">Packet</a>&lt;T, P&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"libits/transport/mqtt/topic/trait.Topic.html\" title=\"trait libits::transport::mqtt::topic::Topic\">Topic</a>,\n    P: <a class=\"trait\" href=\"libits/transport/payload/trait.Payload.html\" title=\"trait libits::transport::payload::Payload\">Payload</a>,</div>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[684]}
