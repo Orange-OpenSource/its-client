2817071ab1fe28c5b22b0d91eedc8e202f94ab25
tigroo <frederic.gardes@orange.com>
2025-03-25 16:30:44 +0000

    init

---
+++ trait.impl/core/fmt/trait.Debug.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["libits",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"libits/client/configuration/configuration_error/enum.ConfigurationError.html\" title=\"enum libits::client::configuration::configuration_error::ConfigurationError\">ConfigurationError</a>"],["impl&lt;T, P&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"libits/transport/packet/struct.Packet.html\" title=\"struct libits::transport::packet::Packet\">Packet</a>&lt;T, P&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"libits/transport/mqtt/topic/trait.Topic.html\" title=\"trait libits::transport::mqtt::topic::Topic\">Topic</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a>,\n    P: <a class=\"trait\" href=\"libits/transport/payload/trait.Payload.html\" title=\"trait libits::transport::payload::Payload\">Payload</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a>,</div>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1256]}
