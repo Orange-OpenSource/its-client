19fdca7ede12fac0776b1726975383db5c2d2ceb
Frédéric Gardes <frederic.gardes@orange.com>
2025-04-15 19:00:32 +0200

    rust: implements the appropriate method
    Fix and rename an exchange spatem test.
    Do not inmplement Content from Informlation:
    we aren"t embedded into a Exchange,
    so we aren't a Message.
    Do not trace not Message exchange (internal stuff).
    Partial migration of MAPEM and SPATEM: harmonize to use stationId.
    TODO : fully implement 2.X.Y versions of the SPATEM and MAPEM messages.
    
    Signed-off-by: Frédéric Gardes <frederic.gardes@orange.com>

--- rust/src/exchange/etsi/cooperative_awareness_message.rs
+++ rust/src/exchange/etsi/cooperative_awareness_message.rs
@@ -115,7 +115,7 @@ impl Content for CooperativeAwarenessMessage {
     /// TODO implement this (issue [#96](https://github.com/Orange-OpenSource/its-client/issues/96))
     fn appropriate(&mut self, timestamp: u64, new_station_id: u32) {
         self.station_id = new_station_id;
-        self.generation_delta_time = timestamp_to_generation_delta_time(timestamp)
+        self.generation_delta_time = timestamp_to_generation_delta_time(timestamp);
     }
 
     fn as_mobile(&self) -> Result<&dyn Mobile, ContentError> {
