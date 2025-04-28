fb554a2d3cb7c7f11b2ea2ffde5bd8e4e40d563c
Frédéric Gardes <frederic.gardes@orange.com>
2025-04-28 12:41:39 +0200

    rust: use the station id as service name into the telemetry
    If the mobility feature isn't activated,
    use "iot3" as service name in the telemetry.
    Test the mqtt username and password.
    Test the telemetry tls.
    Use the station id as ue_id into the bootstrap.
    If the mobility feature isn't activated,
    use "iot3" as ue_id in the bootstrap.
    
    Signed-off-by: Frédéric Gardes <frederic.gardes@orange.com>

--- rust/src/client/bootstrap.rs
+++ rust/src/client/bootstrap.rs
@@ -234,7 +234,6 @@ fn telemetry_configuration_from_bootstrap(
 
 async fn do_bootstrap(
     bootstrap_configuration: BootstrapConfiguration,
-    id: &str,
 ) -> Result<Bootstrap, BootstrapError> {
     info!(
         "Calling bootstrap on '{}'...",
@@ -246,7 +245,7 @@ async fn do_bootstrap(
         .expect("Failed to create telemetry HTTP client");
 
     let body = json!({
-        "ue_id": id,
+        "ue_id": bootstrap_configuration.station_id,
         "psk_login": bootstrap_configuration.username,
         "psk_password": bootstrap_configuration.password,
         "role": bootstrap_configuration.role
