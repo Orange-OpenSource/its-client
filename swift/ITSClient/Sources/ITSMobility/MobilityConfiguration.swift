3891dd4fb081d9688b80b84371a2c177caaf7eb9
François Suc <francois.suc@orange.com>
2025-06-04 09:44:40 +0200

    swift: add a report zoom level setter
    As the report zoom level can be dynamic without restarting the mobility, a new setter is created and the property has been removed from the configuration.
    
    Signed-off-by: François Suc <francois.suc@orange.com>

--- swift/ITSClient/Sources/ITSMobility/MobilityConfiguration.swift
+++ swift/ITSClient/Sources/ITSMobility/MobilityConfiguration.swift
@@ -20,8 +20,6 @@ public struct MobilityConfiguration: Sendable {
     public let stationID: UInt32
     /// The namespace used in the MQTT topic.
     public let namespace: String
-    /// The zoom level used when a position or an alert is sent.
-    public let reportZoomLevel: Int
 
     var userIdentifier: String {
         coreConfiguration.mqttClientConfiguration.clientIdentifier
@@ -32,16 +30,13 @@ public struct MobilityConfiguration: Sendable {
     ///   - coreConfiguration: The `CoreConfiguration` which includes MQTT and telemetry configurations.
     ///   - stationID: The station identifier. Must be unique and the same each time on a same device.
     ///   - namespace: The namespace used in the MQTT topic.
-    ///   - reportZoomLevel: The zoom level used when a position or an alert is sent.
     public init(
         coreConfiguration: CoreConfiguration,
         stationID: UInt32,
         namespace: String = "default",
-        reportZoomLevel: Int = 22
     ) {
         self.coreConfiguration = coreConfiguration
         self.stationID = stationID
         self.namespace = namespace
-        self.reportZoomLevel = reportZoomLevel
     }
 }
