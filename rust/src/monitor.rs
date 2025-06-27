d7f1a122102fd6b9c160e338c0c21d124d741b5a
Frédéric Gardes <frederic.gardes@orange.com>
2025-06-27 15:49:47 +0200

    rust: implement the version 2 of the messages
    Use the new ReferencePosition version
    (one optional field more).
    Check the macro compatibility.
    Manage the old reference position into a dedicated struct.
    Use the old reference position into the (old) current cam.
    Move specific methods to dedicated file (out of the application one).
    Implment specific ETSI shared fields into a dedicated file.
    Implement a legacy 1.1.3 CAM.
    
    Signed-off-by: Frédéric Gardes <frederic.gardes@orange.com>

--- rust/src/monitor.rs
+++ rust/src/monitor.rs
@@ -13,6 +13,7 @@ use crate::exchange::Exchange;
 use crate::exchange::cause::Cause;
 use crate::exchange::etsi::collective_perception_message::CollectivePerceptionMessage;
 use crate::exchange::etsi::cooperative_awareness_message::CooperativeAwarenessMessage;
+use crate::exchange::etsi::cooperative_awareness_message_113::CooperativeAwarenessMessage113;
 use crate::exchange::etsi::decentralized_environmental_notification_message::DecentralizedEnvironmentalNotificationMessage;
 use crate::exchange::etsi::map_extended_message::MAPExtendedMessage;
 use crate::exchange::etsi::signal_phase_and_timing_extended_message::SignalPhaseAndTimingExtendedMessage;
@@ -32,11 +33,12 @@ pub fn trace_exchange(
         Message::CPM(cpm) => format_cpm_trace(cpm),
         Message::MAPEM(map) => format_mapem_trace(map),
         Message::SPATEM(spat) => format_spatem_trace(spat),
+        Message::CAM113(cam) => format_cam_113_trace(cam),
     };
     println!(
         "{} {} {} {} {} at {}",
         component,
-        exchange.type_field,
+        exchange.message_type,
         direction,
         partner,
         message_part,
@@ -44,12 +46,23 @@ pub fn trace_exchange(
     );
 }
 
+fn format_trace(station_id: u32, generation_delta_time: u16) -> String {
+    format!("{}/{}", station_id, generation_delta_time)
+}
+
 pub(crate) fn format_cam_trace(cam: &CooperativeAwarenessMessage) -> String {
-    format!("{}/{}", cam.station_id, cam.generation_delta_time)
+    format_trace(cam.station_id, cam.generation_delta_time)
+}
+
+pub(crate) fn format_cam_113_trace(cam: &CooperativeAwarenessMessage113) -> String {
+    format_trace(cam.station_id, cam.generation_delta_time)
 }
 
 fn format_cpm_trace(cpm: &CollectivePerceptionMessage) -> String {
-    format!("{}/{}", cpm.station_id, cpm.generation_delta_time)
+    format!(
+        "{}/{}",
+        cpm.station_id, cpm.management_container.reference_time
+    )
 }
 
 fn format_denm_trace(
@@ -59,10 +72,10 @@ fn format_denm_trace(
     format!(
         "{}/{}/{}/{}/{}{}",
         denm.station_id,
-        denm.management_container.action_id.originating_station_id,
-        denm.management_container.action_id.sequence_number,
-        denm.management_container.reference_time,
-        denm.management_container.detection_time,
+        denm.management.action_id.originating_station_id,
+        denm.management.action_id.sequence_number,
+        denm.management.reference_time,
+        denm.management.detection_time,
         get_cause_str(cause),
     )
 }
@@ -71,7 +84,7 @@ fn format_mapem_trace(map: &MAPExtendedMessage) -> String {
     format!(
         "{}/{}/{}",
         map.sending_station_id.unwrap_or_default(),
-        map.id,
+        map.station_id,
         map.region.unwrap_or_default(),
     )
 }
@@ -80,7 +93,7 @@ fn format_spatem_trace(spat: &SignalPhaseAndTimingExtendedMessage) -> String {
     format!(
         "{}/{}/{}",
         spat.sending_station_id.unwrap_or_default(),
-        spat.id,
+        spat.station_id,
         spat.region.unwrap_or_default(),
     )
 }
