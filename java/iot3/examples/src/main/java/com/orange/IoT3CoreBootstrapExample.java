665e3be6f1987bea85556fa7c84b1c0d1af7bf78
Mathieu LEFEBVRE <mathieu1.lefebvre@orange.com>
2025-09-25 14:59:03 +0200

    java/iot3examples: add the MQTT keep alive to the IoT3Core example classes
    Signed-off-by: Mathieu LEFEBVRE <mathieu1.lefebvre@orange.com>

--- java/iot3/examples/src/main/java/com/orange/IoT3CoreBootstrapExample.java
+++ java/iot3/examples/src/main/java/com/orange/IoT3CoreBootstrapExample.java
@@ -20,6 +20,8 @@ public class IoT3CoreBootstrapExample {
     private static final BootstrapHelper.Role BOOTSTRAP_ROLE = BootstrapHelper.Role.EXTERNAL_APP;
     private static final String BOOTSTRAP_URI = "bootstrap.uri.com";
 
+    private static final int EXAMPLE_MQTT_KEEP_ALIVE = 30;
+
     private static IoT3Core ioT3Core;
 
     public static void main(String[] args) {
@@ -57,6 +59,7 @@ public class IoT3CoreBootstrapExample {
         // instantiate IoT3Core with the bootstrap configuration
         ioT3Core = new IoT3Core.IoT3CoreBuilder()
                 .bootstrapConfig(bootstrapConfig)
+                .mqttKeepAlive(EXAMPLE_MQTT_KEEP_ALIVE) // optional, default is 60 seconds
                 .callback(new IoT3CoreCallback() {
                     @Override
                     public void mqttConnectionLost(Throwable throwable) {
