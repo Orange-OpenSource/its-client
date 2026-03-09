ec45de56e349e7b32d64d4d8d9729db3b34597d0
Mathieu LEFEBVRE <mathieu1.lefebvre@orange.com>
2026-03-09 10:16:53 +0100

    java/iot3mobility: add copyright to all CAM v1.1.3 and v2.3.0 classes
    Signed-off-by: Mathieu LEFEBVRE <mathieu1.lefebvre@orange.com>

--- java/iot3/mobility/src/main/java/com/orange/iot3mobility/message/cam/v113/validation/CamValidationException.java
+++ java/iot3/mobility/src/main/java/com/orange/iot3mobility/message/cam/v113/validation/CamValidationException.java
@@ -1,3 +1,10 @@
+/*
+ Copyright 2016-2026 Orange
+
+ This software is distributed under the MIT license, see LICENSE.txt file for more details.
+
+ @author Mathieu LEFEBVRE <mathieu1.lefebvre@orange.com>
+ */
 package com.orange.iot3mobility.message.cam.v113.validation;
 
 public final class CamValidationException extends RuntimeException {
--- java/iot3/mobility/src/main/java/com/orange/iot3mobility/message/cam/v230/model/specialvehiclecontainer/DangerousGoodsContainer.java
+++ java/iot3/mobility/src/main/java/com/orange/iot3mobility/message/cam/v230/model/specialvehiclecontainer/DangerousGoodsContainer.java
@@ -1,8 +1,15 @@
+/*
+ Copyright 2016-2026 Orange
+
+ This software is distributed under the MIT license, see LICENSE.txt file for more details.
+
+ @author Mathieu LEFEBVRE <mathieu1.lefebvre@orange.com>
+ */
 package com.orange.iot3mobility.message.cam.v230.model.specialvehiclecontainer;
 
 /**
  * DangerousGoodsContainer v2.3.0
- *
+ * <p>
  * If the vehicleRole component is set to dangerousGoods(3) this container shall be present.
  *
  * @param dangerousGoodsBasic Type of the dangerous goods being carried by a heavy vehicle. explosives1 (0),
