f7eea464a2f0c14f0a7ac3e8594c9c41efec0184
Mathieu LEFEBVRE <mathieu1.lefebvre@orange.com>
2026-03-19 15:10:14 +0100

    java/iot3mobility: add missing copyright to CPM v1.2.1 model classes
    Signed-off-by: Mathieu LEFEBVRE <mathieu1.lefebvre@orange.com>

--- java/iot3/mobility/src/main/java/com/orange/iot3mobility/messages/cpm/v121/model/sensorinformationcontainer/SensorInformationContainer.java
+++ java/iot3/mobility/src/main/java/com/orange/iot3mobility/messages/cpm/v121/model/sensorinformationcontainer/SensorInformationContainer.java
@@ -1,3 +1,10 @@
+/*
+ Copyright 2016-2026 Orange
+
+ This software is distributed under the MIT license, see LICENSE.txt file for more details.
+
+ @author Mathieu LEFEBVRE <mathieu1.lefebvre@orange.com>
+ */
 package com.orange.iot3mobility.messages.cpm.v121.model.sensorinformationcontainer;
 
 import java.util.List;
