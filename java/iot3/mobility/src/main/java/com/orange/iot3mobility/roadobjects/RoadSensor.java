08d6efede01a5c6de1cd1127c3ea9fd1935bcfed
Mathieu LEFEBVRE <mathieu1.lefebvre@orange.com>
2026-03-20 16:05:23 +0100

    java/iot3mobility: add CPM v2.1.1 compatibility to RoadSensor class
    Signed-off-by: Mathieu LEFEBVRE <mathieu1.lefebvre@orange.com>

--- java/iot3/mobility/src/main/java/com/orange/iot3mobility/roadobjects/RoadSensor.java
+++ java/iot3/mobility/src/main/java/com/orange/iot3mobility/roadobjects/RoadSensor.java
@@ -14,6 +14,7 @@ import com.orange.iot3mobility.messages.cpm.core.CpmVersion;
 import com.orange.iot3mobility.messages.cpm.v121.model.CpmEnvelope121;
 import com.orange.iot3mobility.messages.cpm.v121.model.perceivedobjectcontainer.ObjectClassification;
 import com.orange.iot3mobility.messages.cpm.v121.model.perceivedobjectcontainer.PerceivedObject;
+import com.orange.iot3mobility.messages.cpm.v211.model.CpmEnvelope211;
 import com.orange.iot3mobility.quadkey.LatLng;
 import com.orange.iot3mobility.Utils;
 
@@ -100,6 +101,111 @@ public class RoadSensor {
                             perceivedObject.ySpeed());
                     int infoQuality = 3;
 
+                    if(perceivedObject.classification() != null
+                            && !perceivedObject.classification().isEmpty()) {
+                        infoQuality = perceivedObject.classification().get(0).confidence();
+                    }
+
+                    SensorObject sensorObject;
+                    if(sensorObjectMap.containsKey(objectId)) {
+                        // update existing SensorObject
+                        synchronized (sensorObjectMap) {
+                            sensorObject = sensorObjectMap.get(objectId);
+                            if(sensorObject != null) {
+                                sensorObject.updateTimestamp();
+                                sensorObject.setPosition(objectPosition);
+                                sensorObject.setType(objectType);
+                                sensorObject.setSpeed(objectSpeed);
+                                sensorObject.setHeading(objectHeading);
+                                sensorObject.setInfoQuality(infoQuality);
+                                ioT3RoadSensorCallback.sensorObjectUpdate(sensorObject);
+                            }
+                        }
+                    } else {
+                        // create new SensorObject
+                        sensorObject = new SensorObject(objectId, objectType, objectPosition, objectSpeed, objectHeading, infoQuality);
+                        synchronized (sensorObjects) {
+                            sensorObjects.add(sensorObject);
+                        }
+                        synchronized (sensorObjectMap) {
+                            sensorObjectMap.put(objectId, sensorObject);
+                        }
+                        ioT3RoadSensorCallback.newSensorObject(sensorObject);
+                    }
+
+                    if(perceivedObjects.size() != sensorObjects.size()
+                            && !sensorObjects.isEmpty()) {
+                        // remove objects that have not been tracked / detected again
+                        checkAndRemoveExpiredObjects(false);
+                    }
+                    zeroObjectsTimestamp = 0;
+                }
+            } else { // clear all objects if nothing is received for 1.5 seconds
+                if(zeroObjectsTimestamp == 0) zeroObjectsTimestamp = System.currentTimeMillis();
+                else if(System.currentTimeMillis() - zeroObjectsTimestamp > LIFETIME) checkAndRemoveExpiredObjects(true);
+            }
+        } else if (cpmFrame != null && cpmFrame.version() == CpmVersion.V2_1_1) {
+            CpmEnvelope211 cpmEnvelope211 = (CpmEnvelope211) cpmFrame.envelope();
+            List<com.orange.iot3mobility.messages.cpm.v211.model.perceivedobjectcontainer.PerceivedObject> perceivedObjects = new ArrayList<>();
+            if (cpmEnvelope211.message().perceivedObjectContainer() != null) {
+                perceivedObjects = cpmEnvelope211.message().perceivedObjectContainer().perceivedObjects();
+            }
+
+            if(!perceivedObjects.isEmpty()) {
+                for (int index = 0; index < perceivedObjects.size(); index++) {
+                    com.orange.iot3mobility.messages.cpm.v211.model.perceivedobjectcontainer.PerceivedObject perceivedObject = perceivedObjects.get(index);
+                    Integer perceivedObjectId = perceivedObject.objectId();
+                    String objectId = uuid + "_" + (perceivedObjectId != null ? perceivedObjectId : index);
+
+                    if (perceivedObject.position() == null
+                            || perceivedObject.position().xCoordinate() == null
+                            || perceivedObject.position().yCoordinate() == null) {
+                        continue;
+                    }
+
+                    int xDistance = perceivedObject.position().xCoordinate().value();
+                    int yDistance = perceivedObject.position().yCoordinate().value();
+                    double xOffsetMeters = EtsiConverter.cpmDistanceMeters(xDistance);
+                    double yOffsetMeters = EtsiConverter.cpmDistanceMeters(yDistance);
+
+                    LatLng objectPosition = Utils.pointFromPosition(position, 0, yOffsetMeters);
+                    objectPosition = Utils.pointFromPosition(objectPosition, (90 + 360) % 360, xOffsetMeters);
+
+                    SensorObjectType objectType = SensorObjectType.UNKNOWN;
+                    if(perceivedObject.classification() != null) {
+                        for(com.orange.iot3mobility.messages.cpm.v211.model.perceivedobjectcontainer.ObjectClassification classificationItem: perceivedObject.classification()) {
+                            if(classificationItem != null && classificationItem.objectClass() != null) {
+                                objectType = cpm211ObjectTypeFromObjectClass(classificationItem.objectClass());
+                            }
+                        }
+                    }
+
+                    double objectSpeed = 0.0;
+                    double objectHeading = 0.0;
+                    if (perceivedObject.velocity() != null) {
+                        if (perceivedObject.velocity().cartesianVelocity() != null
+                                && perceivedObject.velocity().cartesianVelocity().xVelocity() != null
+                                && perceivedObject.velocity().cartesianVelocity().yVelocity() != null) {
+                            int xSpeed = perceivedObject.velocity().cartesianVelocity().xVelocity().value();
+                            int ySpeed = perceivedObject.velocity().cartesianVelocity().yVelocity().value();
+                            objectSpeed = EtsiConverter.cpmDerivedSpeedMetersPerSecond(xSpeed, ySpeed);
+                            objectHeading = EtsiConverter.cpmDerivedHeadingDegrees(xSpeed, ySpeed);
+                        } else if (perceivedObject.velocity().polarVelocity() != null) {
+                            if (perceivedObject.velocity().polarVelocity().velocityMagnitude() != null) {
+                                objectSpeed = EtsiConverter.cpmSpeedMetersPerSecond(
+                                        perceivedObject.velocity().polarVelocity().velocityMagnitude().value());
+                            }
+                            if (perceivedObject.velocity().polarVelocity().velocityDirection() != null) {
+                                objectHeading = EtsiConverter.cpmAngleDegrees(
+                                        perceivedObject.velocity().polarVelocity().velocityDirection().value());
+                                if (Double.isNaN(objectHeading)) {
+                                    objectHeading = 0.0;
+                                }
+                            }
+                        }
+                    }
+
+                    int infoQuality = 3;
                     if(perceivedObject.classification() != null
                             && !perceivedObject.classification().isEmpty()) {
                         infoQuality = perceivedObject.classification().get(0).confidence();
