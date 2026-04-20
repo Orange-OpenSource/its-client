40d27c9beba3c00fc739ef7e6b7fa9f8caf97c53
Mathieu LEFEBVRE <mathieu1.lefebvre@orange.com>
2026-04-20 14:45:30 +0200

    java/iot3: add author to all test classes headers
    Signed-off-by: Mathieu LEFEBVRE <mathieu1.lefebvre@orange.com>

--- java/iot3/core/src/test/java/com/orange/iot3core/bootstrap/BootstrapConfigTest.java
+++ java/iot3/core/src/test/java/com/orange/iot3core/bootstrap/BootstrapConfigTest.java
@@ -1,7 +1,9 @@
 /*
- Copyright 2016-2025 Orange
+ Copyright 2016-2026 Orange
 
  This software is distributed under the MIT license, see LICENSE.txt file for more details.
+
+ @author Mathieu LEFEBVRE <mathieu1.lefebvre@orange.com>
  */
 package com.orange.iot3core.bootstrap;
 
--- java/iot3/mobility/src/test/java/com/orange/iot3mobility/managers/RoadSensorManagerTest.java
+++ java/iot3/mobility/src/test/java/com/orange/iot3mobility/managers/RoadSensorManagerTest.java
@@ -2,6 +2,8 @@
  Copyright 2016-2026 Orange
 
  This software is distributed under the MIT license, see LICENSE.txt file for more details.
+
+ @author Mathieu LEFEBVRE <mathieu1.lefebvre@orange.com>
  */
 package com.orange.iot3mobility.managers;
 
