a707109a49e2eb01e9bd923dfc9fd495c3ca2622
Frédéric Gardes <frederic.gardes@orange.com>
2026-07-31 12:15:51 +0200

    python/iot3/mobility: compliance with python 3.12
    Since python 3.12, datetime.utcfromtimestamp() is deprecated, and should
    be replaced by calling fromtimestamp() with UTC as timezone [0].
    
    However, the leapseconds module we use behind the scene, expects
    datetime objects without tzinfo. Since this module is not very well
    understood (we copied it from upstream), we don't want to modify it too
    much, so we remove the TZ info from the datetime objects before calling
    into that module.
    
    [0] https://docs.python.org/3.12/library/datetime.html#datetime.datetime.utcfromtimestamp
    
    Signed-off-by: Frédéric Gardes <frederic.gardes@orange.com>
    [yann.morin@orange.com:
    - split it out and into its own commit
    - expand the commit log
    ]
    Signed-off-by: Yann E. MORIN <yann.morin@orange.com>

--- python/iot3/src/iot3/mobility/etsi.py
+++ python/iot3/src/iot3/mobility/etsi.py
@@ -192,7 +192,9 @@ class ETSI(abc.ABC):
         94694401000
         """
         tai_time = leapseconds.utc_to_tai(
-            datetime.datetime.utcfromtimestamp(unix_time)
+            datetime.datetime.fromtimestamp(unix_time, datetime.timezone.utc).replace(
+                tzinfo=None
+            )
         ).timestamp()
         return ETSI.si2etsi(tai_time - ETSI.EPOCH, ETSI.MILLI_SECOND, 0)
 
@@ -219,7 +221,9 @@ class ETSI(abc.ABC):
         """
         tai_time = ETSI.etsi2si(etsi_time, ETSI.MILLI_SECOND, 0) + ETSI.EPOCH
         return leapseconds.tai_to_utc(
-            datetime.datetime.utcfromtimestamp(tai_time)
+            datetime.datetime.fromtimestamp(tai_time, datetime.timezone.utc).replace(
+                tzinfo=None
+            )
         ).timestamp()
 
     @staticmethod
