65668827fa7193f5735b20ee55c8d6f270db0442
Yann E. MORIN <yann.morin@orange.com>
2026-08-19 13:45:03 +0200

    python/its-status: replace general.id with general.station-uuid
    Signed-off-by: Yann E. MORIN <yann.morin@orange.com>

--- python/its-status/src/its_status/collector.static.py
+++ python/its-status/src/its_status/collector.static.py
@@ -3,13 +3,27 @@
 # SPDX-License-Identifier: MIT
 # Author: Yann E. MORIN <yann.morin@orange.com>
 
+import warnings
+
 
 class Status:
     def __init__(self, *, cfg):
+        try:
+            station_uuid = cfg["generic"]["station-uuid"]
+        except KeyError as k_err:
+            # Try the legacy 'id' field
+            try:
+                station_uuid = cfg["generic"]["id"]
+            except KeyError:
+                raise k_err from None
+            warnings.warn(
+                "Using the legacy general.id field; swith to general.station-uuid instead.",
+            )
+
         self.data = {
             "version": "1.2.0",
             "type": "status",
-            "id": cfg["generic"]["id"],
+            "station-uuid": station_uuid,
         }
 
     def capture(self):
